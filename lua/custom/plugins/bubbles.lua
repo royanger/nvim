039fc7e2e1b32ec4ce4074a2d5052921ea4de5b2
Roy Anger <roy@royanger.com>
2023-09-04 00:32:07 -0400

    Updated the lauline theme

--- lua/custom/plugins/bubbles.lua
+++ lua/custom/plugins/bubbles.lua
@@ -22,7 +22,7 @@ return {
           insert = { a = { fg = "#000000", bg = "#80a0ff" } },
           visual = { a = { fg = "#000000", bg = "#79dac8" } },
           replace = { a = { fg = "#000000", bg = "#ff5189" } },
-          command = { a = { fg = "#000000", bg = "#ffffff" } },
+          command = { a = { fg = "#000000", bg = "#f5b041" } },
 
           inactive = {
             a = { fg = "#c6c6c6", bg = "#000000" },
@@ -30,22 +30,36 @@ return {
             c = { fg = "#000000", bg = "#000000" },
           },
         },
-
         component_separators = '|',
-        section_separators = { left = '', right = '' },
       },
       sections = {
         lualine_a = {
-          { 'mode', separator = { left = '' }, right_padding = 2 },
+          { 'mode', separator = { right = '' }, padding = 1 },
         },
-        lualine_b = { 'filename', 'branch', 'diff', 'diagnostics', {
-          -- 'mode', separator = { right = '' }
-        } },
+        lualine_b = {
+          'filename',
+          'diff',
+          'diagnostics',
+          {
+            'branch',
+            separator = { right = '' },
+            right_padding = 2,
+            left_padding = 2
+          } },
         lualine_c = {},
-        lualine_x = { 'searchcount', 'encoding', 'fileformat' },
-        lualine_y = { 'filetype', 'progress' },
+        lualine_x = {},
+        lualine_y = {
+          {
+            'datetime',
+            style = "%H:%M",
+            separator = { left = '' },
+            right_padding = 2,
+            left_padding = 2
+          },
+          'encoding', 'fileformat', 'filetype', 'progress'
+        },
         lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
+          { 'location', separator = { left = '' }, padding = 1 },
         },
       },
       inactive_sections = {
