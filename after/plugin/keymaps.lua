7c631cfe54a0581733b4536855606ac9dfccb810
Roy Anger <roy@royanger.com>
2023-09-04 22:40:08 -0400

    Cleanign up the repo

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,3 +1,12 @@
+-- [[ Basic Keymaps ]]
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+--[[ Workd Wrap keymaps]]
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+
 --[[ Map 'jk' to ESC in Visual and Insert mode ]]
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Esc while in Insert mode" })
 vim.keymap.set("x", "jk", "<ESC>", { desc = "Esc while in Visual mode" })
@@ -76,3 +85,9 @@ vim.keymap.set('n', '<leader>gl', '<Cmd>LazyGit<CR>')
 
 --[[ Git DiffView keymaps]]
 vim.keymap.set('n', '<leader>gv', '<Cmd>DiffviewOpen<CR>')
+
+--[[ Diagnostics keymaps]]
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
---
+++ after/plugin/neo-tree.lua
@@ -0,0 +1,18 @@
+-- Neotree config
+local neotree = require('neo-tree')
+neotree.setup({
+	filesystem = {
+		follow_current_file = {
+			enabled = true
+		},
+		filtered_items = {
+			visible = false,
+			show_hidden_count = true,
+			hide_dotfiles = false,
+			hide_gitignored = false,
+			hide_hidden = false,
+		}
+	}
+})
+
+vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal right toggle<Enter>')
